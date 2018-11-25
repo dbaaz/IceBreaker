6e989be159d57946d84596433bd8b353c42da120
Sumeet Suresh <nfs.sumeet@gmail.com>
2018-11-25 14:36:37 +0530

    Read extended profile data from Firebase

--- app/src/main/java/com/arbiter/droid/icebreakerprot1/ViewProfileActivity.java
+++ app/src/main/java/com/arbiter/droid/icebreakerprot1/ViewProfileActivity.java
@@ -2,23 +2,15 @@ package com.arbiter.droid.icebreakerprot1;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,7 +18,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
 
 import static com.arbiter.droid.icebreakerprot1.Common.setStorageImageToImageView;
 
@@ -38,9 +29,24 @@ public class ViewProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_profile);
         String name;
         final SharedPreferences sharedPrefs = this.getSharedPreferences("Icebreak",0);
+        DatabaseReference fd = FirebaseDatabase.getInstance().getReference().child("users").child(sharedPrefs.getString("saved_name",""));
         this.setTitle(name=getIntent().getStringExtra("name"));
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final TextView bioTextView = findViewById(R.id.textView4);
+        final TextView genTextView = findViewById(R.id.textView9);
         setSupportActionBar(toolbar);
+        fd.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                bioTextView.setText(dataSnapshot.child("bio").getValue().toString());
+                genTextView.setText(dataSnapshot.child("gender").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
